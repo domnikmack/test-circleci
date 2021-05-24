a346cb6c07378c0424b19de1a2a3eca7acae41d8
Domnik Mack <domnikmack@gmail.com>
2021-05-24 13:55:51 -0600

    readme

--- main/hello.py
+++ main/hello.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello! This is Python " + platform.python_version()
+    return "Hello World! This is Python " + platform.python_version()
--- test-results/tests.py
+++
@@ -1,6 +0,0 @@
-from hello import app
-with app.test_client() as c:
-    response = c.get('/')
-    assert response.data.startswith('Hello! This is Python 2')
-    assert not response.data.startswith('Hello! This is Python 3')
-    assert response.status_code == 200
