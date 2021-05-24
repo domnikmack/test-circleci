35e1722bb1b3c2cfff319077308f115546b93bcd
Domnik Mack <domnikmack@gmail.com>
2021-05-24 10:55:30 -0600

    fix hello

--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
 from hello import app
 with app.test_client() as c:
     response = c.get('/')
-    assert response.data.startswith('Hello! This is Python 2')
-    assert response.data.startswith('Hello! This is Python 2')
+    assert response.data.startswith('Hello! This is Python 2.7')
     assert response.status_code == 200
