b10fd422e72b91db8e2c3010f4898ff72252707b
Domnik Mack <domnikmack@gmail.com>
2021-05-24 11:06:58 -0600

    check against py 2 and 3

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from hello import app
 with app.test_client() as c:
     response = c.get('/')
-    assert response.data.startswith('Hello! This is Python 3')
+    assert response.data.startswith('Hello! This is Python 2')
+    assert not response.data.startswith('Hello! This is Python 3')
     assert response.status_code == 200
